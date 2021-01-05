22036182a6bcbf56f59892a30f7109ba7f9194e0
monroekimmel <monroekimmel@gmail.com>
2021-01-04 21:21:45 -0500

    Init Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-component',
+  template: '<h1>Hello, world!</h1>',
+  styles: ["h1 { color: red; }"]
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Hello, World!';
 }
